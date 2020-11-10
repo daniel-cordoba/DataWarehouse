92b9797a2515ff18567f9b8dda8d8cbde647405f
daniel-cordoba <daniel.cordoba13@example.com>
2020-11-10 09:30:13 -0500

    Before DataTable Library

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -33,23 +33,49 @@ async function getContacts(){
     }
 }
 async function fillContacts(){
-    let contacts = await getContacts();
-    console.log(contacts);
-    contacts.forEach(contact => {
-        const name = contact.name;
-        const last_name = contact.last_name;
-        const country = contact.country;
-        const region = contact.region;
-        const email = contact.email;
-        const company = contact.company;
-        const charge = contact.charge;
-        const interest = contact.interest;
-        tableContacts(name, last_name, country, region, email, company, charge, interest);
-    });
-    $(function () {
-        $('[data-toggle="tooltip"]').tooltip()
-    });
+    try {
+        let contacts = await getContacts();
+        console.log(contacts.length);        
+        if (contacts.length == 0) {
+            $(`#pagination`).html(`0 - 0 de 0`);
+        } 
+        $(`#pagination`).html(`1 - 9 de ${contacts.length}`);
+        sessionStorage.setItem("contacts", JSON.stringify(contacts));        
+        //console.log(JSON.parse(sessionStorage.getItem("contacts")));
+        for (let i = 0; i < 9; i++) {
+            const contact = contacts[i];
+            const name = contact.name;
+            const last_name = contact.last_name;
+            const country = contact.country;
+            const region = contact.region;
+            const email = contact.email;
+            const company = contact.company;
+            const charge = contact.charge;
+            const interest = contact.interest;
+            tableContacts(name, last_name, country, region, email, company, charge, interest);        
+        }   
+        
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip()
+        });
+
+       /*  $('#example').DataTable( {
+            columnDefs: [ {
+                orderable: false,
+                className: 'select-checkbox',
+                targets:   0
+            } ],
+            select: {
+                style:    'os',
+                selector: 'td:first-child'
+            },
+            order: [[ 1, 'asc' ]]
+        } ); */
+    } catch (error) {
+        console.error(error);
+    }
 }
+
 function tableContacts(name, last_name, country, region, email, company, charge, interest){    
     $("#table_contacts")
         .append($('<tr>')
@@ -58,12 +84,44 @@ function tableContacts(name, last_name, country, region, email, company, charge,
             .append(`<td><p class="m-0">${country}</p><p class="m-0 font-italic font-weight-light">${region}</p></td>`)
             .append(`<td class="align-middle">${company}</td>`)        
             .append(`<td class="align-middle">${charge}</td>`)
-            .append(`<td class="align-middle"><div class="progress" style="height:20px;"><div class="progress-bar bg-info" role="progressbar" style="width: ${interest};" aria-valuemin="0" aria-valuemax="100">${interest}</div></div></td>`)
+            .append(`<td class="align-middle"><div class="d-flex align-items-center">
+                        <p class="mb-0 mr-1" style="width: 3vw;">${interest}</p> 
+                        <div class="progress" style="height: 10px; width: 80%;">
+                            <div class="progress-bar bg-info font-weight-bold" role="progressbar" style="width: ${interest};" aria-valuenow="${interest}" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        </div>
+                     </td>`)
             .append($('<td class="align-middle text-center">')
                 .append('<button class="btn btn-info mr-1" data-toggle="tooltip" data-placement="top" title="Editar"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/><path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/></svg></button>')
                 .append('<button class="btn btn-info" data-toggle="tooltip" data-placement="top" title="Eliminar"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/><path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/></svg></button>')
                 )
             );
+
+}
+let pag_count = 0;
+function paginationNext(){ 
+    try {
+        pag_count += 9;
+        let contacts = JSON.parse(sessionStorage.getItem("contacts"));        
+        $('#pagination').html(`${pag_count+1} - ${pag_count+10} de ${contacts.length}`);      
+    for (let i = pag_count; i < pag_count+9; i++) {                
+        const contact = contacts[i];
+        const name = contact.name;
+        const last_name = contact.last_name;
+        const country = contact.country;
+        const region = contact.region;
+        const email = contact.email;
+        const company = contact.company;
+        const charge = contact.charge;
+        const interest = contact.interest;
+        tableContacts(name, last_name, country, region, email, company, charge, interest);        
+    }
+    } catch (error) {
+        console.error('Se terminó la tabla');
+        let contacts = JSON.parse(sessionStorage.getItem("contacts"));
+        $('#pagination').html(`${pag_count+1} - ${contacts.length} de ${contacts.length}`); 
+    }
+    
 }
 
 //FUNCION PARA INICIALIZAR LOS TOOLTIPS

1b2e8be1e741c7c9373cbe4f1966098547d17580
daniel-cordoba <daniel.cordoba13@example.com>
2020-11-17 14:42:19 -0500

    DELETE Companies DONE

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -793,7 +793,7 @@ function tableCompanies(ID, name, city, adress) {
             .append(`<td class="align-middle">${adress}</td>`)
             .append($('<td class="align-middle text-center">')
                 .append('<button class="btn btn-info mr-1" data-toggle="modal" data-target="#modal_add_company" title="Editar" onclick="edit_company_btn(this)"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/><path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/></svg></button>')
-                .append('<button class="btn btn-info" data-toggle="modal" data-target="#modal_del_company" onclick="del_company_btn(this)" title="Eliminar"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/><path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/></svg></button>')
+                .append('<button class="btn btn-info" data-toggle="modal" data-target="#modal_company_eliminate" onclick="eliminate_company_icon(this)" title="Eliminar"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/><path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/></svg></button>')
         )
     );
 }
@@ -862,7 +862,7 @@ async function postCompanyData() {
         $('#modal_add_company').modal('hide');
     } catch (error) {console.error(error);}
 }
-/////////////////////////////////////FUNCIONES PARA EDITAR COMPAÑIAS/////////////////////////////////////
+/////////////////////////////////////FUNCIONES PARA EDITAR COMPAÑÍAS/////////////////////////////////////
 //ENDPOINT PUT COMPANY
 async function putCompany(name, adress, email, phone, city, city_id){
     const jwt = sessionStorage.getItem("jwt"); 
@@ -945,6 +945,30 @@ async function data_putCompany(){
         $('#modal_add_company').modal('hide');
     } catch (error) {console.error(error);}
 }
+/////////////////////////////////////FUNCIONES PARA ELIMINAR COMPAÑÍAS/////////////////////////////////////
+//ENDPOINT Eliminate Company
+async function eliminate_company(){
+    const jwt = sessionStorage.getItem("jwt");
+    const ID = sessionStorage.getItem("eliminate_company");
+    if(jwt!=null){
+        let response = await fetch('http://localhost:3000/company/'+ID,
+        {
+            method:'DELETE',
+            headers:{"Authorization":"Bearer "+jwt}
+        });
+        let eliminado = await response.json();
+        console.log(eliminado);
+        $('#table_companies').find(`[data-id='${ID}']`)[0].remove();
+    }
+}
+function eliminate_company_icon(btn) {
+    console.log(btn.parentElement.parentElement.getAttribute("data-id"));
+    let ID = btn.parentElement.parentElement.getAttribute("data-id");    
+    sessionStorage.setItem("eliminate_company", ID); 
+}
+    
+
+
 
 
 function pruebas() {
